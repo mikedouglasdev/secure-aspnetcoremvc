9b74d1b50364b7468a08f33b3224075f5285aacb
Mike Douglas <mikedouglas@deliveron.com>
2020-04-19 15:22:39 -0500

    initial commit

---
+++ secureaspnetcoremvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace secureaspnetcoremvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
