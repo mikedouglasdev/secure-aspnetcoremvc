65cb88d92978720ddf72a4ad33925446d2ae9886
Mike Douglas <mikedouglas@deliveron.com>
2020-04-25 16:25:59 -0500

    hard code header

--- secureaspnetcoremvc/Startup.cs
+++ secureaspnetcoremvc/Startup.cs
@@ -54,6 +54,15 @@ namespace secureaspnetcoremvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use((context, next) =>
+            {
+                context.Response.Headers["Cache-Control"] = "no-cache";
+                context.Response.Headers["Pragma"] = "no-cache";
+                return next.Invoke();
+            });
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
